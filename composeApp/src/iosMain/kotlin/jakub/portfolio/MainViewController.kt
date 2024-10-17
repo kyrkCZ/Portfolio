9f8c689ed2087d18d5c77afef00815fdc1915e8b
Jakub Kramný <57757403+kyrkCZ@users.noreply.github.com>
2024-10-17 21:01:46 +0200

    Initial commit

---
+++ composeApp/src/iosMain/kotlin/jakub/portfolio/MainViewController.kt
@@ -0,0 +1,5 @@
+package jakub.portfolio
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/wasmJsMain/kotlin/jakub/portfolio/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package jakub.portfolio
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
